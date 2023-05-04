7e1f6ddb17955266e1414d6cca1b6662ccd1314d
koneneru <koneneru@gmail.com>
2023-05-04 17:22:04 +0400

    Added FakeCloudProviderClass with minimum code

---
+++ SyncEngine/Source Files/Provider/FakeCloudProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyncEngine
+{
+	public class FakeCloudProvider
+	{
+		public static bool Start(in string serverFolder = null, in string clientFolder = null)
+		{
+			bool result = false;
+
+			if (ProviderFolderLocations.Init(serverFolder, clientFolder))
+			{
+
+				result = true;
+			}
+
+			return result;
+		}
+	}
+}
